18e8a01332b5239cfa183750657865c6ad0e7f0c
derwentx <derwentx@gmail.com>
2017-03-31 11:19:53 +1100

    better meta handling and tests

--- gpm_cache.py
+++ gpm_cache.py
@@ -27,6 +27,82 @@ DEBUG_LEVELS = {
 
 FILENAME_KEEP_CHARS = (' ', '.', '_')
 
+class TrackInfo(object):
+    """Helper class stores information about a track."""
+
+    def __init__(self, track_id, track_info):
+        if not track_info:
+            track_info = {}
+        self.track_id = track_id
+        self.track_info = track_info
+
+    @property
+    def id3_meta(self):
+        """Return the id3 tags for this object."""
+
+        # possible keys:  'albumartistsort', 'musicbrainz_albumstatus',
+        # 'lyricist', 'musicbrainz_workid', 'releasecountry', 'date',
+        # 'albumartist', 'musicbrainz_albumartistid', 'composer', 'catalognumber',
+        # 'encodedby', 'tracknumber', 'musicbrainz_albumid', 'album', 'asin',
+        # 'musicbrainz_artistid', 'mood', 'copyright', 'author', 'media',
+        # 'performer', 'length', 'acoustid_fingerprint', 'version', 'artistsort',
+        # 'titlesort', 'discsubtitle', 'website', 'musicip_fingerprint',
+        # 'conductor', 'musicbrainz_releasegroupid', 'compilation', 'barcode',
+        # 'performer:*', 'composersort', 'musicbrainz_discid',
+        # 'musicbrainz_albumtype', 'genre', 'isrc', 'discnumber',
+        # 'musicbrainz_trmid', 'acoustid_id', 'replaygain_*_gain', 'musicip_puid',
+        # 'originaldate', 'language', 'artist', 'title', 'bpm',
+        # 'musicbrainz_trackid', 'arranger', 'albumsort', 'replaygain_*_peak',
+        # 'organization', 'musicbrainz_releasetrackid'
+
+        response = {}
+        for meta_key, info_key, mapping_fn in [
+                ('artist', 'artist', str),
+                ('albumartist', 'albumArtist', str),
+                ('title', 'title', str),
+                ('album', 'album', str),
+                ('genre', 'genre', str),
+                ('tracknumber', 'trackNumber', str),
+                ('discnumber', 'discNumber', str),
+                ('date', 'year', str),
+
+        ]:
+            if self.track_info.get(info_key) is not None:
+                response[meta_key] = mapping_fn(self.track_info.get(info_key))
+
+        return response
+
+    @property
+    def filing_artist(self):
+        """Return the artist under which this track should be filed."""
+        filing_artists = \
+            [
+                self.track_info.get(key) for key in \
+                ['albumArtist', 'artist', 'composer'] \
+                if self.track_info.get(key)
+            ]
+        return filing_artists[0] if filing_artists else "Unknown Artist"
+
+    @property
+    def filing_album(self):
+        response = self.track_info.get('album') if self.track_info.get('album') else "Unkown Album"
+        # if self.track_info.get('discNumber'):
+        #     response = "%s - disc %s" % (response, self.track_info.get('discNumber'))
+        if self.track_info.get('year'):
+            response = "%s [%s]" % (response, self.track_info.get('year'))
+        return response
+
+    @property
+    def filing_title(self):
+        response = self.track_info.get('title') if self.track_info.get('title') else self.track_id
+        if self.track_info.get('trackNumber') is not None:
+            response = "%02d - %s" % (self.track_info.get('trackNumber'), response)
+            if self.track_info.get('discNumber') is not None:
+                response = "%02d:%s" % (self.track_info.get('discNumber'), response)
+        return response
+
+
+
 # pprint(EasyID3.valid_keys.keys())
 def get_parser_args():
     """Parse arguments from cli, env and config files."""
@@ -54,7 +130,8 @@ def get_parser_args():
     parser.add_argument(
         '--sleep-time',
         help="Time to sleep in between requests",
-        default=10
+        default=10,
+        type=float
     )
 
     # TODO: implement playlist-cached
@@ -86,7 +163,7 @@ def to_safe_filename(thing):
     """Take a stringable object and return an ASCII string safe for filenames."""
     return "".join(c for c in str(thing) if c.isalnum() or c in FILENAME_KEEP_CHARS).rstrip()
 
-def save_meta(local_filepath, track_info):
+def save_meta(local_filepath, track_info=None):
     """Save meta associated with track to the given file."""
 
     try:
@@ -95,67 +172,58 @@ def save_meta(local_filepath, track_info):
         meta = mutagen.File(local_filepath, easy=True)
         meta.add_tags()
 
-    for meta_key, info_key in [
-            ('artist', 'artist'),
-            ('albumartist', 'albumArtist'),
-            ('title', 'title'),
-            ('album', 'album'),
-            ('genre', 'genre')
-    ]:
-        if track_info.get(info_key) is not None:
-            meta[meta_key] = track_info.get(info_key)
+    for meta_key, meta_val in track_info.id3_meta.items():
+        meta[meta_key] = meta_val
 
-    # if track_info.get('trackNumber') is not None:
-    #     meta['tracknumber'] = track_info.get('trackNumber')
-    # if track_info.get('discNumber') is not None:
-    #     meta['discnumber'] = track_info.get('discNumber')
-    # TODO: meta['date']
     meta.save()
 
-def cache_track(api, parser_args, track_id, track_info=None):
+def get_local_filepath(parser_args, track_id, track_info=None):
     """
-    Cache a single track from the API.
+    Determine the best location to save the track on disk.
+
+    Create dirs if necessary.
     """
 
-    if not track_info:
-        track_info = {}
-
-    filing_artists = \
-        [
-            track_info.get(key) for key in \
-            ['albumArtist', 'artist', 'composer'] \
-            if track_info.get(key)
-        ]
-    filing_artist = filing_artists[0] if filing_artists else "Unknown Artist"
-    filing_album = track_info.get('album') if track_info.get('album') else "Unkown Album"
-    filing_title = track_info.get('title') if track_info.get('title') else track_id
-
-    local_filepath = "%s.mp3" % to_safe_filename(filing_title)
-    if track_info.get('trackNumber') is not None:
-        local_filepath = "%02d - %s" % (track_info.get('trackNumber'), local_filepath)
-    if track_info.get('discNumber') is not None and track_info.get('trackNumber') is not None:
-        local_filepath = "%02d:%s" % (track_info.get('discNumber'), local_filepath)
-    local_dir = os.path.join(filing_artist, filing_album)
-    local_dir = os.path.join(parser_args.cache_location, local_dir)
-    local_dir = os.path.expanduser(local_dir)
+    local_filepath = "%s.mp3" % to_safe_filename(track_info.filing_title)
+    local_dir = os.path.expanduser(
+        os.path.join(*[
+            parser_args.cache_location,
+            to_safe_filename(track_info.filing_artist),
+            to_safe_filename(track_info.filing_album)
+        ])
+    )
+    # local_dir = os.path.join(track_info.filing_artist, track_info.filing_album)
+    # local_dir = os.path.join(parser_args.cache_location, local_dir)
+    # local_dir = os.path.expanduser(local_dir)
     local_filepath = os.path.join(local_dir, local_filepath)
 
     logging.info(
         "caching song: id %s; artist %s; album %s; title %s; path %s",
         to_safe_print(track_id),
-        to_safe_print(filing_artist),
-        to_safe_print(filing_album),
-        to_safe_print(filing_title),
+        to_safe_print(track_info.filing_artist),
+        to_safe_print(track_info.filing_album),
+        to_safe_print(track_info.filing_title),
         to_safe_print(local_filepath)
     )
 
     if not os.path.exists(local_dir):
         os.makedirs(local_dir)
 
+    return local_filepath
+
+def cache_track(api, parser_args, track_id, track_info=None):
+    """
+    Cache a single track from the API.
+    """
+
+    info_obj = TrackInfo(track_id, track_info)
+
     cache_url = api.get_stream_url(track_id)
     logging.info("cache_url: %s", to_safe_print(cache_url))
 
     req = requests.get(cache_url, stream=True)
+
+    local_filepath = get_local_filepath(parser_args, track_id, info_obj)
 
     with open(local_filepath, 'wb') as loc_file:
         for chunk in req.iter_content(chunk_size=1024):
@@ -164,10 +232,10 @@ def cache_track(api, parser_args, track_id, track_info=None):
         logging.info("wrote file %s", loc_file.name)
         loc_file.flush()
 
-    save_meta(local_filepath, track_info)
+    save_meta(local_filepath, info_obj)
 
     logging.info("taking a nap")
-    time.sleep(parser_args.sleep_time)
+    time.sleep(float(parser_args.sleep_time))
 
     return local_filepath
 
@@ -195,11 +263,22 @@ def cache_playlist(api, parser_args):
     if target_playlist:
         logging.info("found target playlist %s", pformat(target_playlist))
 
+        failed_tracks = []
+
         for track in target_playlist['tracks']:
             track_id = track['trackId']
             track_info = track.get('track')
-            filename = cache_track(api, parser_args, track_id, track_info)
-            logging.info("succesfully cacheed to %s", to_safe_print(filename))
+            try:
+                filename = cache_track(api, parser_args, track_id, track_info)
+                logging.info("succesfully cacheed to %s", to_safe_print(filename))
+            except mutagen.mp3.HeaderNotFoundError:
+                failed_tracks.append(track)
+                logging.info("\n\n!!! failed to cache track, %s. info: %s", track_id, track_info)
+
+        if failed_tracks:
+            logging.warning("tracks that failed")
+            for track in failed_tracks:
+                logging.warning("-> %s %s", track['trackID'], track['track'])
     else:
         logging.warning("no playlist matched search string: %s", repr(parser_args.playlist))
 
--- tests.py
+++ tests.py
@@ -4,7 +4,7 @@ Basic tests for gpm_cache.
 """
 
 import unittest
-from gpm_cache import to_safe_print, to_safe_filename
+from gpm_cache import to_safe_print, to_safe_filename, TrackInfo
 
 class TestHelpers(unittest.TestCase):
     """Test helper functions of gpm_cache."""
@@ -24,6 +24,94 @@ class TestHelpers(unittest.TestCase):
     def test_safe_filename(self):
         response = to_safe_filename(u"hellö#my~baby")
         self.assertEqual(response, "hellmybaby")
+
+class TestTrackInfo(unittest.TestCase):
+    """Test TrackInfo helper class."""
+
+    track_info = {
+        u'album': u'Southpaw EP',
+        u'albumArtRef': [
+            {
+                u'aspectRatio': u'1',
+                u'autogen': False,
+                u'kind': u'sj#imageRef',
+                u'url': \
+(u'http://lh3.googleusercontent.com/'
+ u'JPIzh3RwQ8E3tuW9iiCd8SSA7xSDGwHeplgP2d903AmibiDpX5_zkPa2rDFsYheTkeyi_nA7lw')
+            }
+        ],
+        u'albumArtist': u'Moonbase Commander',
+        u'albumAvailableForPurchase': False,
+        u'albumId': u'B52ucw7kgew7axvye5vldk6rxty',
+        u'artist': u'Moonbase Commander',
+        u'artistArtRef': [
+            {
+                u'aspectRatio': u'2',
+                u'autogen': True,
+                u'kind': u'sj#imageRef',
+                u'url': \
+(u'http://lh3.googleusercontent.com/'
+ u'Y7bfPNOK1Y2besTzlMMPq_IbMyxf0B3lP_nNfa41CCxaF0MyBYDknVxu6cZO-RrPEXVrrAJRyg')
+            },
+            {
+                u'aspectRatio': u'1',
+                u'autogen': True,
+                u'kind': u'sj#imageRef',
+                u'url': \
+(u'http://lh3.googleusercontent.com/'
+ u'qa82ufTF4bZLbveox1RoR93yCwM60hbXO5NvUH91gjR8KLLSHCeNnxXbjU_TtQTYiFQyPA_voQ')
+            }
+        ],
+        u'artistId': [u'Aefmiddcllvc6xda7wyj7w3r3yy'],
+        u'composer': u'',
+        u'discNumber': 1,
+        u'durationMillis': u'213000',
+        u'estimatedSize': u'8557841',
+        u'explicitType': u'2',
+        u'genre': u'Dance/Electronic',
+        u'kind': u'sj#track',
+        u'nid': u'6zn3up2um24dxoggvac6obj7ay',
+        u'playCount': 1,
+        u'storeId': u'T6zn3up2um24dxoggvac6obj7ay',
+        u'title': u'Southpaw',
+        u'trackAvailableForPurchase': True,
+        u'trackAvailableForSubscription': True,
+        u'trackNumber': 1,
+        u'trackType': u'7',
+        u'year': 2015
+    }
+
+    def test_basic(self):
+        info_obj = TrackInfo(
+            track_id=u'T6zn3up2um24dxoggvac6obj7ay',
+            track_info=self.track_info
+        )
+
+        self.assertEquals(
+            info_obj.id3_meta,
+            {
+                'album': 'Southpaw EP',
+                'title': 'Southpaw',
+                'artist': 'Moonbase Commander',
+                'albumartist': 'Moonbase Commander',
+                'date': '2015',
+                'genre': 'Dance/Electronic',
+                'discnumber': '1',
+                'tracknumber': '1'
+            }
+        )
+        self.assertEqual(
+            info_obj.filing_album,
+            'Southpaw EP [2015]'
+        )
+        self.assertEqual(
+            info_obj.filing_artist,
+            'Moonbase Commander'
+        )
+        self.assertEqual(
+            info_obj.filing_title,
+            '01:01 - Southpaw'
+        )
 
 if __name__ == '__main__':
     unittest.main()
